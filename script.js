c91183cd63bf9c78e1811e9bd4091797d03cc9d0
Brendan Fung <brendan.fung@edu.sait.ca>
2024-11-07 18:17:02 -0700

    changed console

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 sheep.addEventListener('click', function () {
-    console.log("moose image")
+    console.log("sheepimage")
     document.getElementById('image').src = 'Images/sheep.webp';
 });
 
 bear.addEventListener('click', function () {
-    console.log("bear image")
+    console.log("bearimage")
     document.getElementById('image').src = 'Images/bear.webp';
 });
 
 moose.addEventListener('click', function () {
-    console.log("moose image")
+    console.log("mooseimage")
     document.getElementById('image').src = 'Images/moose.webp';
 });
 
 wolf.addEventListener('click', function () {
-    console.log("moose image")
+    console.log("wolfimage")
     document.getElementById('image').src = 'Images/wolf.webp';
 });
 
 elk.addEventListener('click', function () {
-    console.log("moose image")
+    console.log("elkimage")
     document.getElementById('image').src = 'Images/elk.webp';
 });
 
